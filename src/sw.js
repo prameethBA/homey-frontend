cf22db430caca00337e6c8fb92a3de9c49e4e4d5
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-08-31 10:27:01 +0530

    serviceWorker customized

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,23 @@
+const filesToCache = [
+    '/',
+    'assets/css/reset.css',
+    'index.html',
+    'favicon.ico',
+    'app.js'
+]
+
+const staticCacheName = 'pages-cache-v1'
+
 // Install the SW
 self.addEventListener('install', event => {
     console.log('Insatalled')
     self.skipWaiting()
+
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            return cache.addAll(filesToCache)
+        })
+    )
 })
 
 // Activate the SW
@@ -9,7 +25,24 @@ self.addEventListener('activate', event => console.log('Activated '))
 
 // Listen for fetch
 self.addEventListener('fetch', event => {
-    console.log('Fetching:', event.request.url)
+    console.log('Fetch event for ', event.request.url)
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache')
+                    return response
+                }
+                console.log('Network request for ', event.request.url)
+                return fetch(event.request)
+
+                // TODO 4 - Add fetched files to the cache
+            })
+            .catch(error => {
+                // TODO 6 - Respond with custom offline page
+            })
+    )
 })
 
 // Listen for push
