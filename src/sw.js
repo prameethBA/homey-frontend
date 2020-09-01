7ca0390419b6b3a0171ec6ef31aeb17bab1efe98
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-09-01 17:36:49 +0530

    Update SW

--- src/sw.js
+++ src/sw.js
@@ -25,19 +25,23 @@ self.addEventListener('activate', event => console.log('Activated '))
 
 // Listen for fetch
 self.addEventListener('fetch', event => {
-    console.log('Fetch event for ', event.request.url)
+    // console.log('Fetch event for ', event.request.url)
     event.respondWith(
         caches
             .match(event.request)
             .then(response => {
                 if (response) {
-                    console.log('Found ', event.request.url, ' in cache')
+                    // console.log('Found ', event.request.url, ' in cache')
                     return response
                 }
-                console.log('Network request for ', event.request.url)
-                return fetch(event.request)
-
-                // TODO 4 - Add fetched files to the cache
+                // console.log('Network request for ', event.request.url)
+                return fetch(event.request).then(response => {
+                    // TODO 5 - Respond with custom 404 page
+                    return caches.open(staticCacheName).then(cache => {
+                        cache.put(event.request.url, response.clone())
+                        return response
+                    })
+                })
             })
             .catch(error => {
                 // TODO 6 - Respond with custom offline page
