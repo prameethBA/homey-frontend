6d0bd8836fbba935405ad77f76b739d178ca2e1f
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-10-10 23:53:25 +0530

    update Base Componet

--- src/componets/Base.js
+++ src/componets/Base.js
@@ -3,13 +3,18 @@ export default class Base extends HTMLElement {
 
     state = {}
 
-    styled(style) {
-        if (style === null) return ' '
-        return '<style>' + style + '</style>'
+    styled() {
+        if (this.style === null) return ' '
+        return '<style>' + this.style + '</style>'
     }
-    render(style, content) {
-        if (content === null) content = ''
-        this.template.innerHTML = this.styled(style) + content
+    render() {
+        if (this.content === null) this.content = ''
+        this.template.innerHTML = this.styled() + this.content
+    }
+    mount() {
+        this.render()
+        this.attachShadow({ mode: 'open' })
+        this.shadowRoot.appendChild(this.template.content.cloneNode(true))
     }
     setPath(path) {
         window.history.pushState({}, '', path)
