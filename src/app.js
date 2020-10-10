b8112816b42d7ca44187de6d598d4384e23d0366
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-10-10 23:53:50 +0530

    Change componets

--- src/app.js
+++ src/app.js
@@ -12,124 +12,123 @@ router.get('/', async () => {
   console.log('home')
 })
 
-const style = `
-
-    #mainContainer {
-        padding:0;
-        margin:0;
-        width:100%;
-        height:100%;
-    }
-
-    .container {
-        display: flex;
-        width:100%;
-        height:100%;
-        flex-direction: row;
-        justify-content: center;
-        position: absolute;
-        top: 80%;
-        left: 50%;
-        z-index: 1;
-        transform: translate(-50%, -50%);
-    }
-
-    .container > user-comp {
-        padding: 1em;
-    }
-
-    #login-form {
-        display: none;
-    }
-
-    user-comp {
-        cursor: pointer;
-    }
-
-`
-const content = `
-<navigation-bar></navigation-bar>
-    <div id="mainContainer">
-        <div class="container">
-            <user-comp mirror="true" route="/">
-                <img slot="image" defer src="https://media.istockphoto.com/photos/for-rent-sign-in-front-of-new-house-picture-id149060607?k=6&m=149060607&s=612x612&w=0&h=9CQCG-T1Oq2vgBjUEJbxny1OqJAbs6FpbhTQZK36Lxg=" alt="Image"></img>
-                <h1 slot="title">Rent or Lease your own property</h1>
-            </user-comp>
-            <user-comp mirror="true" route="/">
-                <img slot="image" defer src="https://s3.amazonaws.com/clients.granalacantadvertiser.images/wp-content/uploads/2017/06/14072232/2236775_2_O.jpg" alt="Image"></img>
-                <h1 slot="title">Looking for a place</h1>
-            </user-comp>
-        </div>
-    <div>
-    <login-form id="login-form"></login-form>
-<footer-c></footer-c>
-`
 
 class UI extends Base {
+  
+  style = `
+  
+      #mainContainer {
+          padding:0;
+          margin:0;
+          width:100%;
+          height:100%;
+      }
+  
+      .container {
+          display: flex;
+          width:100%;
+          height:100%;
+          flex-direction: row;
+          justify-content: center;
+          position: absolute;
+          top: 80%;
+          left: 50%;
+          z-index: 1;
+          transform: translate(-50%, -50%);
+      }
+  
+      .container > user-comp {
+          padding: 1em;
+      }
+  
+      #login-form {
+          display: none;
+      }
+  
+      user-comp {
+          cursor: pointer;
+      }
+  
+  `
+  content = `
+  <navigation-bar></navigation-bar>
+      <div id="mainContainer">
+          <div class="container">
+              <user-comp mirror="true" route="/">
+                  <img slot="image" defer src="https://media.istockphoto.com/photos/for-rent-sign-in-front-of-new-house-picture-id149060607?k=6&m=149060607&s=612x612&w=0&h=9CQCG-T1Oq2vgBjUEJbxny1OqJAbs6FpbhTQZK36Lxg=" alt="Image"></img>
+                  <h1 slot="title">Rent or Lease your own property</h1>
+              </user-comp>
+              <user-comp mirror="true" route="/">
+                  <img slot="image" defer src="https://s3.amazonaws.com/clients.granalacantadvertiser.images/wp-content/uploads/2017/06/14072232/2236775_2_O.jpg" alt="Image"></img>
+                  <h1 slot="title">Looking for a place</h1>
+              </user-comp>
+          </div>
+      <div>
+      <login-form id="login-form"></login-form>
+  <footer-c></footer-c>
+  `
+  
   constructor() {
     super()
+    this.mount()
 
-    this.render(style, content)
-    this.attachShadow({ mode: 'open' })
-    this.shadowRoot.appendChild(this.template.content.cloneNode(true))
+  //   const exitForm = () => {
+  //     addEventListener('exit-login-form', () => {
+  //       this.shadowRoot.querySelector('#login-form').style.display = 'none'
+  //     })
+  //   }
 
-    const exitForm = () => {
-      addEventListener('exit-login-form', () => {
-        this.shadowRoot.querySelector('#login-form').style.display = 'none'
-      })
-    }
+  //   addEventListener('login-form', async () => {
+  //     this.setPath('/login')
+  //     await import('./componets/home/login-form.js')
+  //     this.shadowRoot.querySelector('#login-form').style.display = 'flex'
 
-    addEventListener('login-form', async () => {
-      this.setPath('/login')
-      await import('./componets/home/login-form.js')
-      this.shadowRoot.querySelector('#login-form').style.display = 'flex'
+  //     exitForm()
+  //   })
 
-      exitForm()
-    })
+  //   router.get('/signup', async () => {
+  //     await import('./componets/home/login-form.js')
+  //     this.shadowRoot.querySelector('#login-form').style.display = 'flex'
+  //     dispatchEvent(new Event('signup-form'))
 
-    router.get('/signup', async () => {
-      await import('./componets/home/login-form.js')
-      this.shadowRoot.querySelector('#login-form').style.display = 'flex'
-      dispatchEvent(new Event('signup-form'))
+  //     exitForm()
+  //   })
 
-      exitForm()
-    })
+  //   router.get('/reset-password', async () => {
+  //     await import('./componets/home/login-form.js')
+  //     this.shadowRoot.querySelector('#login-form').style.display = 'flex'
+  //     dispatchEvent(new Event('reset-password-form'))
 
-    router.get('/reset-password', async () => {
-      await import('./componets/home/login-form.js')
-      this.shadowRoot.querySelector('#login-form').style.display = 'flex'
-      dispatchEvent(new Event('reset-password-form'))
+  //     exitForm()
+  //   })
+  // }
 
-      exitForm()
-    })
-  }
+  // connectedCallback() {
+  //   //This is used for developing purpose only
+  //   router.get('/add-property', async () => {
+  //     await import('./componets/property/add-property.js')
+  //     this.shadowRoot.querySelector(
+  //       '#mainContainer'
+  //     ).innerHTML = `<add-property></add-property>`
+  //   })
 
-  connectedCallback() {
-    //This is used for developing purpose only
-    router.get('/add-property', async () => {
-      await import('./componets/property/add-property.js')
-      this.shadowRoot.querySelector(
-        '#mainContainer'
-      ).innerHTML = `<add-property></add-property>`
-    })
+  //   addEventListener('reload-home', () => {
+  //     this.render(style, content)
+  //     this.shadowRoot.innerHTML = ''
+  //     this.shadowRoot.append(this.template.content)
 
-    addEventListener('reload-home', () => {
-      this.render(style, content)
-      this.shadowRoot.innerHTML = ''
-      this.shadowRoot.append(this.template.content)
-
-      this.shadowRoot.querySelectorAll('user-comp').forEach((item) =>
-        item.addEventListener('click', async () => {
-          this.setPath(item.getAttribute('route'))
-          // if (localStorage.login === true) {
-          await import('./componets/user/primary-user.js')
-          this.shadowRoot.querySelector(
-            '#mainContainer'
-          ).innerHTML = `<primary-user></primary-user>`
-          // } else dispatchEvent(new Event("login-form"));
-        })
-      )
-    })
+  //     this.shadowRoot.querySelectorAll('user-comp').forEach((item) =>
+  //       item.addEventListener('click', async () => {
+  //         this.setPath(item.getAttribute('route'))
+  //         // if (localStorage.login === true) {
+  //         await import('./componets/user/primary-user.js')
+  //         this.shadowRoot.querySelector(
+  //           '#mainContainer'
+  //         ).innerHTML = `<primary-user></primary-user>`
+  //         // } else dispatchEvent(new Event("login-form"));
+  //       })
+  //     )
+  //   })
 
     // this.shadowRoot.querySelectorAll('user-comp').forEach((item) =>
     //   item.addEventListener('click', async () => {
--- src/componets/home/navigation-bar.js
+++ src/componets/home/navigation-bar.js
@@ -1,82 +1,80 @@
 import Base from './../Base.js'
 
-const style = `
-
-*{
-  margin: 0;
-  padding: 0;
-  font-family: Century Gothic;
-}
-
-
-ul{
-  float:right;
-  list-style-type:none;
-}
-ul li{
-  display: inline-block;
-  cursor: pointer;
-}
-ul li{
-  text-decoration:none;
-  color:#001f3f;
-  padding:10px 0px;
-  border:5px solid transparent;
-  transition:1s ease;
-}
-
-li:hover {
-  background: url("data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB2ZXJzaW9uPSIxLjEi%0D%0AIGlkPSJMYXllcl8xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhs%0D%0AaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB4PSIwcHgiIHk9IjBweCIKCSB3aWR0%0D%0AaD0iMzkwcHgiIGhlaWdodD0iNTBweCIgdmlld0JveD0iMCAwIDM5MCA1MCIgZW5hYmxlLWJhY2tn%0D%0Acm91bmQ9Im5ldyAwIDAgMzkwIDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4KPHBhdGggZmlsbD0i%0D%0Abm9uZSIgc3Ryb2tlPSIjZDk0ZjVjIiBzdHJva2Utd2lkdGg9IjEuNSIgc3Ryb2tlLW1pdGVybGlt%0D%0AaXQ9IjEwIiBkPSJNMCw0Ny41ODVjMCwwLDk3LjUsMCwxMzAsMAoJYzEzLjc1LDAsMjguNzQtMzgu%0D%0ANzc4LDQ2LjE2OC0xOS40MTZDMTkyLjY2OSw0Ni41LDI0My42MDMsNDcuNTg1LDI2MCw0Ny41ODVj%0D%0AMzEuODIxLDAsMTMwLDAsMTMwLDAiLz4KPC9zdmc+Cg==");
-  animation: line 2s;
-}
-
-li:hover a {
-  color: #d94f5c;
-}
-
-li:not(:last-child) {
-  margin-right: 30px;
-}
-
-@keyframes line {
-  0% {
-    background-position-x: 390px;
-  }
-
-#login-button {
-        height: 2.5em;
-        width: 10em;
-        margin: auto 2em;
-        text-transform: uppercase;
-        color: #eeeeee;
-        background-color: #239710;
-        border: none;
-        box-shadow: 1px 1px 5px 1px rgba(23,97,10,0.64);
-        outline: none;
-        cursor: pointer;
-        transition: 0.4s;
-    }
-    
-    #login-button:hover {
-        background-color: #34a832;
-    }
-
-`
-const content = `
-    <header>
-        <nav>
-            <h3 class="logo">Homey</h3>
-            <button id="login-button"> login </button>
-        </nav
-    </header>
-`
-
 export default class Nav extends Base {
+
+  style = `
+  
+  *{
+    margin: 0;
+    padding: 0;
+    font-family: Century Gothic;
+  }
+  
+  
+  ul{
+    float:right;
+    list-style-type:none;
+  }
+  ul li{
+    display: inline-block;
+    cursor: pointer;
+  }
+  ul li{
+    text-decoration:none;
+    color:#001f3f;
+    padding:10px 0px;
+    border:5px solid transparent;
+    transition:1s ease;
+  }
+  
+  li:hover {
+    background: url("data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB2ZXJzaW9uPSIxLjEi%0D%0AIGlkPSJMYXllcl8xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhs%0D%0AaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB4PSIwcHgiIHk9IjBweCIKCSB3aWR0%0D%0AaD0iMzkwcHgiIGhlaWdodD0iNTBweCIgdmlld0JveD0iMCAwIDM5MCA1MCIgZW5hYmxlLWJhY2tn%0D%0Acm91bmQ9Im5ldyAwIDAgMzkwIDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4KPHBhdGggZmlsbD0i%0D%0Abm9uZSIgc3Ryb2tlPSIjZDk0ZjVjIiBzdHJva2Utd2lkdGg9IjEuNSIgc3Ryb2tlLW1pdGVybGlt%0D%0AaXQ9IjEwIiBkPSJNMCw0Ny41ODVjMCwwLDk3LjUsMCwxMzAsMAoJYzEzLjc1LDAsMjguNzQtMzgu%0D%0ANzc4LDQ2LjE2OC0xOS40MTZDMTkyLjY2OSw0Ni41LDI0My42MDMsNDcuNTg1LDI2MCw0Ny41ODVj%0D%0AMzEuODIxLDAsMTMwLDAsMTMwLDAiLz4KPC9zdmc+Cg==");
+    animation: line 2s;
+  }
+  
+  li:hover a {
+    color: #d94f5c;
+  }
+  
+  li:not(:last-child) {
+    margin-right: 30px;
+  }
+  
+  @keyframes line {
+    0% {
+      background-position-x: 390px;
+    }
+  
+  #login-button {
+          height: 2.5em;
+          width: 10em;
+          margin: auto 2em;
+          text-transform: uppercase;
+          color: #eeeeee;
+          background-color: #239710;
+          border: none;
+          box-shadow: 1px 1px 5px 1px rgba(23,97,10,0.64);
+          outline: none;
+          cursor: pointer;
+          transition: 0.4s;
+      }
+      
+      #login-button:hover {
+          background-color: #34a832;
+      }
+  
+  `
+  content = `
+      <header>
+          <nav>
+              <h3 class="logo">Homey</h3>
+              <button id="login-button"> login </button>
+          </nav
+      </header>
+  `
   constructor() {
     super()
-    this.render(style, content)
-    this.attachShadow({ mode: 'open' })
-    this.shadowRoot.appendChild(this.template.content.cloneNode(true))
+    this.mount()
   }
 
   connectedCallback() {
