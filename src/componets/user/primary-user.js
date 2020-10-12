8e9cef092ed53d20694b9a3d8d398fbaed7d8c7b
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-10-12 23:11:05 +0530

    Update property component with dynamic data allocation

--- src/componets/user/primary-user.js
+++ src/componets/user/primary-user.js
@@ -15,12 +15,13 @@ export default class PrimaryUser extends Base {
         super()
         this.mount()
 
-        const dataArray  = ['Seaview Luxury Double Bedroom for Rent at Border Dehiwala', 'Apartment in Wellwatte']
+        const dataArray  = ['Seaview Luxury Double Bedroom for Rent at Border DehiwalaSeaview Luxury Double Bedroom for Rent at Border Dehiwala', 'Apartment in Wellwatte Near safeplace inface to Gallface']
         let data = '';
         for (let index = 0; index < dataArray.length; index++) {
+            let title = dataArray[index].length>75 ? dataArray[index].substr(0, 75) + `...` : dataArray[index]
             data += `
             <property-view>
-                <h3 slot="title">` + dataArray[index] + `</h3>
+                <h4 slot="title">` + title + `</h4>
             </property-view>
             `
         }
