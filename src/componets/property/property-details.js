03359dccfef205edf19131d206f7c5e3784dcb16
prameeth98 <alice@email.com2018is049@std.ucsc.cmb.ac.lk>
2020-10-15 11:42:48 +0530

    change stylings for property-details UI

--- src/componets/property/property-details.js
+++ src/componets/property/property-details.js
@@ -5,99 +5,97 @@ export default class PropertyDetails extends Base {
   css =  `
   @import url("https://pro.fontawesome.com/releases/v5.10.0/css/all.css");
 
-  *{
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: Arial, Helvetica, sans-serif;
-}
-img {
+  }
+  img {
     max-width: 100%;
     display: block;
-}
-
-/****************
-Typography
-****************/
-
-label{
-    font-weight: bold;
-}
-/**********************
-Layout 
-**************************/
-body {
+  }
+  
+  /****************
+  Typography
+  ****************/
+  
+  /**********************
+  Layout 
+  **************************/
+  body {
     padding-top: 30px;
-    
-}
-.container {
+  }
+  .container {
     width: 90%;
     max-width: 767.98px;
     margin: auto;
-    background: #D1CEE0;
+    color: #fff;
+    background: #001f3f; /*dont need for transparent background: ;*/
     padding: 30px;
-}
-
-
-/*Property select area */
-.properties form{
+  }
+  
+  /*colors
+      orange #ff4000
+      green #34a832
+      dark blue  #001f3f
+  
+  /*Property select area */
+  .properties form {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-}
-
-
-.select_properties{
+  }
+  
+  .select_properties {
     width: 100%;
     display: inline-flex;
     flex-wrap: wrap;
     justify-content: space-between;
     margin-bottom: 20px;
-}
-.select_property{
+  }
+  .select_property {
     width: 15%;
-}
-.select_property input{
+  }
+  .select_property input {
     margin-right: 15px;
-}
-
-
-
- /* about */
- .container_about {
-     display: flex;
-     flex-wrap: wrap;
-     justify-content: space-between;
- }
- .container_about aside {
-     width: 32%;
- }
- .container_about main {
-     width: 65%;
- }
- .about_main_image{
-     margin-bottom: 20px;
- }
- .about_sub_images {
-     display: flex;
-     justify-content: space-between;
- }
- .about_sub_image {
-     width: 20%;
- }
-
- .about_section {
-     display: flex;
-     flex-wrap: wrap;
- }
- .about_section_details{
+  }
+  
+  /* about */
+  .container_about {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+  .container_about aside {
+    width: 32%;
+  }
+  .container_about main {
+    width: 65%;
+  }
+  .about_main_image {
+    margin-bottom: 20px;
+  }
+  .about_sub_images {
+    display: flex;
+    justify-content: space-between;
+  }
+  .about_sub_image {
+    width: 20%;
+  }
+  
+  .about_section {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .about_section_details {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
     margin-bottom: 10px;
     width: 100%;
- }
- .about_icons{
+    color: #ff4000;
+  }
+  .about_icons {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-evenly;
@@ -105,39 +103,48 @@ body {
     width: 40%;
     margin: auto;
     margin-bottom: 10px;
- }
- .about-p{
-     margin-bottom: 20px;
- }
- .about_section_details h2 {
-     margin-bottom: 20px;
- }
- 
-.select_property_about {
+  }
+  .about-p {
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    margin-bottom: 20px;
+  }
+  .about_section_details h2 {
+    margin-bottom: 20px;
+    display: block;
+  }
+  
+  .select_property_about {
+    /*font: 1em verdana;*/
     width: 25%;
-}
-.select_property_about input{
+  }
+  .select_property_about input {
     margin-right: 15px;
-}
-
-
-/* *********************
-Components
- **************************/
-/* Buttons */
-
-
-
-
-
-
-
-.btn {
+  }
+  .select_property_about label {
+    color: grey;
+  }
+  input:checked + label {
+    color: #ff4000;
+  }
+  :checked {
+    margin-left: 2px;
+  }
+  
+  input[type='checkbox']:checked {
+    box-shadow: 0 0 0 2px #ff4000;
+  }
+  
+  /* *********************
+  Components
+   **************************/
+  /* Buttons */
+  
+  .btn {
     display: block;
     border: none;
-   
+  
     font-size: 16px;
-    font-weight:bolder;
+    font-weight: bolder;
     cursor: pointer;
     text-align: center;
     text-decoration: none;
@@ -145,67 +152,49 @@ Components
     color: #fff;
     padding: 0.5em 2em;
     border-radius: 20px;
-    transition: .3s;
+    transition: 0.3s;
     text-transform: uppercase;
-}
-.btn-primary{
-    border: 2px solid #3c73ff;
-    background-color: #3c73ff;
-   
-}
-.btn-primary:hover {
-    box-shadow: 8px 8px #99bdff;
-    transition: .3s;
   }
-
-.btn-secondary{
-    background-color: #C266D3;
-   
-}
-.btn-secondary:hover {
-    box-shadow: 8px 8px #dfa7e9;
-    transition: .3s;
-}
-.btn-success{
-    background-color: #25a18e;
-   
-}
-.btn-danger{
-    background-color: #E00880;
-   
-}
-.btn-danger:hover {
-    box-shadow: 8px 8px #f87cc0;
-    transition: .3s;
-}
-.btn-upload {
-    border-radius:0 10px 10px 0;
-    background:#444857;
-}
-.btn-lg{
-
-    width: 100%;
-}
-.btn-md{
-
-    width: 60%;
-}
-.btn-sm{
-
+  .btn-primary {
+    border: 2px solid #34a832;
+    background-color: #34a832;
+  }
+  .btn-primary:hover {
+    color: #001f3f;
+    box-shadow: 4px 4px #ff4000;
+    transition: 0.5s;
+  }
+  
+  .btn-secondary {
+    background-color: #34a832;
+  }
+  .btn-secondary:hover {
+    color: #001f3f;
+    box-shadow: 4px 4px #ff4000;
+    transition: 0.5s;
+  }
+  .btn-success {
+    background-color: #34a832;
+  }
+  .btn-success:hover {
+    color: #001f3f;
+    box-shadow: 4px 4px #ff4000;
+    transition: 0.5s;
+  }
+  .btn-sm {
     width: 28%;
-    font-size: .8rem;
-}
-.btn-group{
+    font-size: 0.8rem;
+  }
+  .btn-group {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-evenly;
     width: 100%;
-}
-.btn-group-bw{
+  }
+  .btn-group-bw {
     justify-content: space-between;
-}
-
-
+  }
+  
 
   `
 
