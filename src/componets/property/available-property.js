2ef9dbb0e1aa5da0befd459354e6e6731a5b0743
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-10-31 05:51:02 +0530

    Image retriving method ==:

--- src/componets/property/available-property.js
+++ src/componets/property/available-property.js
@@ -71,9 +71,6 @@ export default class AvalibaleProperty extends Base {
         for (let index = 0; index < this.state.limit; index++) {
             this._qs('#container').innerHTML += `
                     <property-view id="id-${index}" key="${index}">
-                        <img class='thumbnail thumbnail-${index}' slot='thumbnail' src="./assets/img/1.png" />
-                        <img class='thumbnail thumbnail-${index}' slot='thumbnail' src="./assets/img/house.jpg" />
-                        <img class='thumbnail thumbnail-${index}' slot='thumbnail' src="./assets/img/background.jpg" />
                         <p slot="title" class=" title title-${index}">Title</p>
                         <p slot="price" class=" price price-${index}">Price</p>
                         <p slot="description" class=" description description-${index}">Description</p>
@@ -87,17 +84,38 @@ export default class AvalibaleProperty extends Base {
 
     const fetchAdds = async (limit, page, find='find') => {
         this.setLoader()
+        this.state.ids = []
         await axios.get(`http://homey-api.atwebpages.com/property/all/${limit}/${page}`)
-            .then(res => res.data.forEach((item, index) =>{
-                this.stopLoader()
-                this._qs(`.title-${index}`).innerHTML  = item.title
-                this._qs(`.price-${index}`).innerHTML  = 'Rs.' + item.price.replace(/\B(?=(\d{3})+(?!\d))/, ",")
-                this._qs(`.description-${index}`).innerHTML  = item.description
-            }))
+            .then(async res => {
+                    res.data.forEach((item, index) =>{
+                    this.stopLoader()
+                    this.state.ids.push(item._id)
+                    this._qs(`#id-${index}`).id = item._id
+                    this._qs(`.title-${index}`).innerHTML  = item.title
+                    this._qs(`.price-${index}`).innerHTML  = 'Rs.' + item.price.replace(/\B(?=(\d{3})+(?!\d))/, ",")
+                    this._qs(`.description-${index}`).innerHTML  = item.description                
+                })
+                
+                await axios.post(`http://homey-api.atwebpages.com/property/images`,{'ids':this.state.ids})
+                    .then(res => {
+                        const data = JSON.parse(res.data).data[0]
+                        const len = (Object.keys(data).length)
+                        for (let loop = 0; loop < len; loop++) {
+                            for (let index = 0; index < data[loop].images.length; index++) {
+                                for (let index2 = 0; index2 < Object.keys(data[loop].images[index]).length; index2++) {
+                                    data[loop].images[index][index2]
+                                    this._qs(`#${data[loop].id}`).innerHTML += `
+                                        <img class='thumbnail' slot='thumbnail' src="${data[loop].images[index]}" />
+                                        `
+                                }
+                                }
+                        }
+                    })
+                })
                 .catch(err => dispatchEvent(new CustomEvent("pop-up", { detail: { pop: 'error', msg: err } })))
         }
         
-        fetchAdds('overview', '')
+        fetchAdds(10, 0)
         
     }//End of connected callback
 
