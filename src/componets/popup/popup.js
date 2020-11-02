0418cbc854b227f09ef2f8ea1a2a8de15c1d1146
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-11-02 21:55:41 +0530

    update popup with timer

--- src/componets/popup/popup.js
+++ src/componets/popup/popup.js
@@ -1,68 +1,18 @@
 import Base from './../Base.js'
+import CSS from './popup.css.js'
 
 export default class error extends Base {
 
-  css = `
-  .container {
-    z-index: 1000;
-    position: fixed;
-    left: 0;
-    top: 1rem;
-    right: 0;
-    background-color: red;
-    background-image: linear-gradient(to right top, #eb5d5d, #e85b5b, #e45958, #e15656, #de5454);
-    width: 90%;
-    border-radius: 3px;
-    margin: auto;
-    transition: all 2s ease;
-  }
-
-  .onsuccess {
-    background-color: green;
-    background-image: linear-gradient(to right top, #24da37, #31de4a, #3ce35a, #47e769, #51eb77);
-  }
-
-  .onnotice {
-    background-color: gray;
-    background-image: linear-gradient(to right top, #b2b2b2, #9c9b9c, #878586, #737071, #605b5b);
-  }
-
-  .oninfo {
-    background-color: lightblue;
-    background-image: linear-gradient(to right top, #0066ff, #0076ff, #0084ff, #0091ff, #009dff);
-  }
-
-  ::slotted(div) {
-      padding: 1em;
-      text-align: center;
-      color: #eeeeee;
-  }
-
-  #close-popup {
-    color: #ffffff;
-    display: inline-block;
-    font-size: 2em;
-    transform: rotate(45deg);
-    position: absolute;
-    right: 0.1em;
-    top: -0.3em;
-    cursor: pointer;
-  }
-
-  .title {
-      margin: 1em 0.5em;
-      color: #ffffff;
-      font-weight: bold;
-  }
-`
+  css = CSS
 
   content = `
     <div class="container">
         <span class="title"> Error </span>
-        <span id="close-popup">+</span>
+        <span id="close-popup">‹</span>
         <hr />
-            <slot name="message"></slot>
+        <slot name="message"></slot>
         <hr />
+        <hr class="loading" />
     </div>
 `
   constructor() {
@@ -96,12 +46,17 @@ export default class error extends Base {
       this._qs('.container').style.top = '-100%'
     })
 
-    console.log(this.state.duration)
-    
-    setTimeout(() => {
-      this._qs('.container').style.top = '-100%'
-    }, this.state.duration);
-  }
+    let widthvalue = 0
+    const loader =  setInterval(() => {
+      this._qs('.loading').style.width = widthvalue +'%'
+      widthvalue ++
+      if(widthvalue > 100) { 
+        clearInterval(loader)
+        this._qs('.container').style.top = '-100%'
+      }
+    }, this.state.duration / 100);
+
+  }//End of the connectedCallback()
 
 }// End of Class
 
