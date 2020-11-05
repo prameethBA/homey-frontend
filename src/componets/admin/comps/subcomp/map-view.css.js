85edcc046b2c610a5b5b535634a8e9971bd24e16
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-11-05 15:00:58 +0530

    Fix UI issues

--- src/componets/admin/comps/subcomp/map-view.css.js
+++ src/componets/admin/comps/subcomp/map-view.css.js
@@ -3,32 +3,20 @@ export default CSS = `
 .container {
     display: flex;
     flex-direction: column;
+    width: 90%;
+    height: 25rem;
 }
 
 #map {
-    display: none;
-    min-width: 50vw;
     width: 100%;
-    min-height: 50vh;
     height: 100%;
-    margin: 0;
+    display: block;
+    position: relative;
+    overflow: hidden;
   }
 
   .gmnoprint {
     display: none;
-  }
-
-  #pac-input {
-    display: none;
-    z-index: 0;
-    position: absolute;
-    margin: 1rem;
-    background-color: rgb(255, 255, 255);
-    border: 1px solid rgb(0, 0, 0);
-    width: 75%;
-    left: 20px;
-    top: 0px;
-    color: #000000;
   }
 
   button {
@@ -40,7 +28,7 @@ export default CSS = `
     cursor: pointer;
     background-image: linear-gradient(to right, #32be8f, #38d39f, #32be8f);
     transition: all 1s;
-    margin: 1rem;
+    margin: 1rem 0;
   }
 
 `
