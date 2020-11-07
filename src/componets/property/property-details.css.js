126d3a3711530ab3a097746d132ce1e939925e9f
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-11-07 18:33:59 +0530

    Restructure property details comp

--- src/componets/property/property-details.css.js
+++ src/componets/property/property-details.css.js
@@ -1,4 +1,131 @@
 export default CSS = `
-  
+    .container {
+      margin: 5rem auto;
+      display: flex;
+      flex-direction: row;
+    }
+
+    .images {
+      width: 75%;
+      margin: 2rem;
+    }
+
+    img {
+      cursor: zoom-in;
+    }
+
+    .main-image-container {
+      width: 100%;
+    }
+
+    .main-image {
+      width: 100%;
+      border: 0.5rem solid #ffffff;
+      border-radius: 2px;
+    }
+
+    .sub-images {
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+    }
+
+    .sub-image {
+      width: 20%;
+      border: 0.1rem solid #ffffff;
+      border-radius: 2px;
+    }
+
+    .price {
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
+
+    .details {
+      margin: 2rem;
+    }
+
+    .details .row {
+      margin: 0 0 1rem 0;
+    }
+
+    .rwo-title {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .title {
+      font-weight: bold;
+      font-size: 1.2rem;
+    }
+
+    .row-status {
+      display: flex;
+      justify-content: space-around;
+    }
+
+    .description {
+      font-size: 0.9rem;
+      line-height: 1rem;
+      max-height: 3rem;
+      overflow: hidden;
+      text-align: justify;
+    }
+
+    .description::first-line {
+      text-indent: 1rem;
+    }
+
+    .features {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      flex-wrap: wrap;
+    }
+    
+    .contact-info {
+      display: flex;
+      justify-content: flex-end;
+    }
+
+    .contace-info a {
+      text-transform: lowercase;
+      color: #2206f7;
+      text-decoration: underline;
+      cursor: pointer;
+    }
+
+    .action {
+      display: flex;
+      justify-content: space-around;
+      margin-top: 5rem;
+    }
+
+    button {
+      width: 30%;
+      height: 2.5em;
+      border-radius: 2rem;
+      outline: none;
+      border: none;
+      background-image: linear-gradient(to right, #32be8f, #38d39f, #32be8f);
+      font-size: 1.2rem;
+      color: aliceblue;
+      text-transform: uppercase;
+      margin: 2rem auto;
+      cursor: pointer;
+      transition: all 1s;
+  }
+
+  button:hover{
+      box-shadow: 1px 1px 5px 0px rgba(0,62,21,0.8);
+  }
+
+  .reserve {
+    background-image: linear-gradient(to right top, #133b77, #00398e, #0036a3, #002eb6, #1520c6);
+  }
+
+  .feedback {
+    background-image: linear-gradient(to right top, #aa7c0e, #ad7308, #af6906, #b15f08, #b3540d);
+  }
 
   `
--- src/componets/property/property-details.js
+++ src/componets/property/property-details.js
@@ -7,6 +7,58 @@ export default class PropertyDetails extends Base {
 
   content = `
     <div class="container">
+
+      <div class="images">
+        <div class="main-image-container">
+          <img class="main-image" src="/assets/img/house.jpg" />
+        </div>
+        <div class="sub-images">
+          <img class="sub-image" src="/assets/img/background.jpg" />
+          <img class="sub-image" src="/assets/img/mountain.jpg" />
+          <img class="sub-image" src="/assets/img/1.png" />
+          <img class="sub-image" src="/assets/img/house.jpg" />
+        </div>
+      </div>
+
+      <div class="details">
+        <div class="row rwo-title">
+          <div class="title">Boarding place in Nugegoda</div>
+          <div class="price">Rs.17,000/Month</div>
+        </div>
+        <div class="row row-status">
+          <div class="status">Avalible 🟢</div>
+          <div class="favourite">⭐</div>
+          <div class="share">📩</div>
+        </div>
+        <div class="row">
+          <div class="description">
+            \/ matches the character / literally (case sensitive)\w* matches any word character (equal to [a-zA-Z0-9_])* Quantifier — Matches between zero and unlimited times, as many times as possible, giving back as needed
+          </div>
+        </div>
+        <div class="row">
+          <div class="features">
+            <span>feature 1</span>
+            <span>feature 1</span>
+            <span>feature 1</span>
+            <span>feature 1</span>
+            <span>feature 1</span>
+            <span>feature 1</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="contact-info">
+            <span> <a>Show contacts </a></span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="action">
+            <button class="reserve"> Reserve Now! </button>
+            <button class="feedback"> Feedback </button>
+            <button class="map"> On map 📌</button>
+          </div>
+        </div>
+      </div>
+
     </div/
   `
     constructor() {
