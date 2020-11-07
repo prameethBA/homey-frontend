afb19acf7ccba325bac05acd4ec4aafa842db04f
Prameeth Maduwantha <64423592+prameethBA@users.noreply.github.com>
2020-11-07 23:17:12 +0530

    Update report component UI

---
+++ src/componets/admin/comps/report-comp.js
@@ -0,0 +1,33 @@
+
+
+
+
+content = `
+<div class="container">
+        <div class="options">
+            <button class="btn-approve">Reviewed Reports</button>
+            <button class="btn-reject">Pending Reports</button>
+        </div>
+        <div class="report-comp">
+            <table id="report-comp-table">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Discrption</th>
+                        <th>Reported User ID</th>
+                        <th>Property Owner ID</th>
+                        <th>Reviewed</th>
+                        <th>Pending</th>
+                    </tr>
+                </thead>
+                <tbody id="report-comp-table-body">
+                    
+                </tbody>
+            </table>
+            </div>
+            <div class="pagination">
+                <a class="previous">First</a> | <a>1</a> | <a>2</a> | <a class="current">3</a> | <a>4</a> | <a>5</a> |<a class="last">Last</a>
+            </div>
+        </div>
+    <div class="preview-advertisement"></div>
+    `
