90813af52028f9c1617e1469d19c53390594e8c6
Prameeth Maduwantha <64423592+prameethBA@users.noreply.github.com>
2020-11-08 16:12:40 +0530

    Update report-comp.js

--- src/componets/admin/comps/report-comp.js
+++ src/componets/admin/comps/report-comp.js
@@ -1,9 +1,12 @@
+import Base from '/componets/Base.js'
+import CSS from './report-comp.css.js'
 
+export default class Report extends Base {
 
+    css = CSS
 
-
-content = `
-<div class="container">
+    content = `
+    <div class="container">
         <div class="options">
             <button class="btn-approve">Reviewed Reports</button>
             <button class="btn-reject">Pending Reports</button>
@@ -31,3 +34,40 @@ content = `
         </div>
     <div class="preview-advertisement"></div>
     `
+
+    constructor() {
+        super()
+        this.mount()
+        
+    }//End of constructor
+
+    //close the dock
+    close() {
+        this._qs('#close-popup').addEventListener('click', () => {
+            this.exitDock()
+        })
+    }//End of the close()
+
+    // Exit the dock
+    exitDock() {
+        this._qs('.backdrop').style.opacity = '0'
+        this._qs('.backdrop').style.pointerEvents = 'none'
+    }// End of exitDock()
+
+    //Exit with Escape key
+    exitWithEscape() {
+        addEventListener('keyup', ({key}) => (key === 'Escape') ? this.exitDock() : null )
+    }// End of exitWithEscape()
+
+    connectedCallback() {
+
+        // close the dock
+        this.close()
+        // Exit with escape key
+        this.exitWithEscape()
+
+    }//End of connectedCallback
+
+}//End of Class
+
+window.customElements.define('report-comp', Report)
