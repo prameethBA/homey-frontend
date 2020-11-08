11e9d74e4729724f46093d9b57fc8292b805ecfa
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-11-08 12:57:31 +0530

    report componet added

---
+++ src/componets/property/subcomp/report/report.css.js
@@ -0,0 +1,43 @@
+export default CSS = `
+    .backdrop {
+        position: fixed;
+        scroll-behavior: auto;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: rgba(0,0,0, 0.7);
+        z-index: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: all 0.5s ease-in-out;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        background-color: #ffffff;
+        min-width: 60%;
+        min-height: 60%;
+        padding: 2rem;
+        border-radius: 2px;
+        max-width: 90%;
+        transition: all 0.5s ease-in-out;
+    }
+
+    #close-popup {
+        color: #444444;
+        font-size: 3rem;
+        transform: rotate(45deg);
+        cursor: pointer;
+        position: absolute;
+        right: 20%;
+        margin-top: -1rem;
+    }
+
+    .row {
+        margin: 1rem 0;
+    }
+
+`
