57aa71ddd9f79f88d3d3fa6ded2027e2f17756e8
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-11-08 16:32:08 +0530

    Chane UI of profile

--- src/componets/account/profile.js
+++ src/componets/account/profile.js
@@ -7,70 +7,61 @@ export default class Profiel extends Base {
 
 
   content = `
-<div class="container container_profile">
-        <aside>
-            <div class="profile_pic">
-                <img src="../assets/img/profile2.png" alt="">
-                <h3>Full Name</h3>
+    <div class="container">
+        <div class="row">
+            <div class="column">
+                <div class="profile-picture-container">
+                    <img  class="profile-picture" src="/assets/img/11.png" />
+                </div>
+                <div class="name-container">
+                    <span class="name">Dimuthu Lakmal</span>
+                </div>
+                <div class="last-login-container">
+                    <span class="last-login-date">2020-10-20</span>
+                    <span class="last-login-time">@ 15:43:23</span>
+                    <span class="last-location">From Sri Lanka</span>
+                    <span class="show-more"><a>Show More...</a></span>
+                </div>
             </div>
-            <div class="profile_details">
-                <article class="profile_detail">
-                   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, cum odit velit quaerat, a cumque cupiditate ipsam iure iusto</p> 
-                </article>
-                <article class="profile_detail">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, cum odit velit quaerat, a cumque cupiditate ipsam iure iusto</p> 
-                </article>
-                <article class="profile_detail">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, cum odit velit quaerat, a cumque cupiditate ipsam iure iusto</p> 
-                </article>
-                <article class="profile_detail">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, cum odit velit quaerat, a cumque cupiditate ipsam iure iusto</p> 
-                </article>
-            </div>
-        </aside>
-        <main>
-            <form action="">
-                <div class="profile-item-two">
-                    <label for="">First Name</label>
-                    <input type="text">
-                </div>
-                <div class="profile-item-two">
-                    <label for="">Last Name</label>
-                    <input type="text">
-                </div>
-                <div class="profile-item-two">
-                    <label for="">NIC</label>
-                    <input type="text">
-                </div>
-                <div class="profile-item-two">
-                    <label for="">Mobile No</label>
-                    <input type="text">
-                </div>
-                <div class="profile-item-one">
-                    <label for="">Email</label>
-                    <input type="text">
-                </div>
-                <div class="profile-item-one">
-                    <label for="">User Detail</label>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
-                </div>
-                <div class="image_property">
-                    <label for="">Profile picture</label>
-                    <div class="image_upload image_upload-lg">
-                        <label for="upload-photo"></label>
-                        <input type="file" name="photo" id="upload-photo" />
-                        <input type="submit"value="Upload" class="btn btn-upload btn-sm">
+            <div class="column">
+                <div class="form">
+                    <div class="row">
+                        <div class="column">
+                            <label for="firstName">First Name</label>
+                            <input type="text" id="firstName" value="Dimuthu" />
+                            <label for="LastName">First Name</label>
+                            <input type="text" id="LastName" value="Lakmal" />
+                        </div>
                     </div>
-                    
+                    <div class="row">
+                        <label for="address-1">Address Line 1</label>
+                        <input type="text" id="address-1" value="No.141" />
+                        <label for="address-2">Address Line 2</label>
+                        <input type="text" id="address-1" value="" />
+                        <label for="address-3">Address Line 3</label>
+                        <input type="text" id="address-3" value="Mediyawa" />
+                        <div class="column">
+                            <label for="city">City</label>
+                            <input type="text" id="city" value="Eppawala" />
+                            <label for="district">District</label>
+                            <input type="text" id="district" value="Anuradhapura" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label for="nic">NIC</label>
+                        <input type="text" id="nic" value="981421485V" />
+                    </div>
+                    <div class="row">
+                        <label >Date of birth</label>
+                        <div class="column">
+                            <input type="text" id="year" value="1998" />
+                            <label for="month">May</label>
+                            <input type="text" id="day" value="21" />
+                        </div>
+                    </div>
                 </div>
-                <div class="btn-group">
-                    <button class="btn btn-primary btn-sm">Save</button>
-                    <a href="" class="btn btn-secondary btn-sm ">Cancel</a>
-
-                </div>
-               
-            </form>
-        </main>
+            </div>
+        </div>
     </div>
 `
     constructor() {
