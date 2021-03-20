c07a69c5bd36489e250cb3bcea7124a0f690c917
Prameeth Maduwantha <64423592+prameethBA@users.noreply.github.com>
2021-03-20 15:45:50 +0530

    Create share module

---
+++ src/componets/universal/share/share.js
@@ -0,0 +1,42 @@
+import Base from "/componets/Base.js";
+import CSS from "./share.css.js";
+
+export default class Share extends Base {
+  css = CSS;
+
+  content = `
+    <div class="backdrop">
+            <div class="social-btns">
+                <a class="btn facebook" href="#"><img src="fb_32px.png" class="fa fa-facebook"></i></a>
+                <a class="btn twitter" href="#"><img src="twitter_32px.png" class="fa fa-twitter"></a>
+                <a class="btn whatsapp" href="#"><img src="whatsapp_32px.png" class="fa fa-whatsapp"></a>
+            </div>
+        </div>
+`;
+
+  constructor() {
+    super();
+    this.mount();
+  } //End of the constructor
+
+  // Exit the dock
+  exitDock() {
+    this._qs(".backdrop").style.opacity = "0";
+    this._qs(".backdrop").style.pointerEvents = "none";
+  } // End of exitDock()
+
+  //Exit with Escape key
+  exitWithEscape() {
+    addEventListener("keyup", ({ key }) =>
+      key === "Escape" ? this.exitDock() : null
+    );
+  } // End of exitWithEscape()
+
+  //connectedCallback
+  connectedCallback() {
+    // Exit with escape key
+    this.exitWithEscape();
+  } //End of connectedCallback()
+} //End of Class
+
+window.customElements.define("share-comp", Share);
