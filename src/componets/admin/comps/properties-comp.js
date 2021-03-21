79732fbcb757098e660416611ac243e275e3c948
Prameeth Maduwantha <64423592+prameethBA@users.noreply.github.com>
2021-03-21 13:47:34 +0530

    Update user-comp.js

--- src/componets/admin/comps/properties-comp.js
+++ src/componets/admin/comps/properties-comp.js
@@ -63,7 +63,6 @@ export default class properties extends Base {
                     </property-view>
                     `
                 })
-                this._qs('#pagination').innerHTML = this.pagination
             }
         } catch (err) {
             console.log(err)
--- src/componets/admin/comps/users-comp.js
+++ src/componets/admin/comps/users-comp.js
@@ -1,11 +1,11 @@
-import Base from './../../Base.js'
-import CSS from './users-comp.css.js'
-import '/componets/universal/pagination/pagination.js'
+import Base from "./../../Base.js";
+import CSS from "./users-comp.css.js";
+import "/componets/universal/pagination/pagination.js";
 
 export default class users extends Base {
-    css = CSS
+  css = CSS;
 
-    content = `
+  content = `
     <div class="container">
         <div class="row">
             <span class="search-container">
@@ -28,45 +28,45 @@ export default class users extends Base {
     </div>
 
     <div class="popup"></div>
-    `
+    `;
 
-    constructor() {
-        super()
-        this.mount()
-    } //End of the constructor
+  constructor() {
+    super();
+    this.mount();
+  } //End of the constructor
 
-    //Load user component
-    loadUser(user) {
-        let data = `
+  //Load user component
+  loadUser(user) {
+    let data = `
             <div class="profile">
                 <div class="sub-row">
                     <img data-id="${user.userId}" id="img-${user.userId}" class="display-picture view-profile" src="/assets/img/alt/load-post.gif" />
                 </div>
                 <div class="sub-row">
                     <span class="name view-profile" data-id="${user.userId}">${user.firstName} ${user.lastName}</span>
-                    <span class="status">`
+                    <span class="status">`;
 
-        switch (user.status) {
-            case '0':
-                data += `🟠 Unconfirmed`
-                break
-            case '1':
-                data += `🟢 Confirmed`
-                break
-            default:
-                data += `🔴 Invalid User`
-                break
-        }
+    switch (user.status) {
+      case "0":
+        data += `🟠 Unconfirmed`;
+        break;
+      case "1":
+        data += `🟢 Confirmed`;
+        break;
+      default:
+        data += `🔴 Invalid User`;
+        break;
+    }
 
-        data += `</span>
+    data += `</span>
                 </div>
                 <div class="sub-row">
                     <span class="email"><a href="mailto:${user.email}">${
-            user.email
-        }<a></span>
+      user.email
+    }<a></span>
                     <span class="mobile"><a href="callto:${user.mobile}">${
-            user.mobile != null ? user.mobile : 'Mobile number not updated'
-        }</a></span>
+      user.mobile != null ? user.mobile : "Mobile number not updated"
+    }</a></span>
                 </div>
                 <div class="sub-row button-group-user">
                     <button class="primary-button">Deactivate</button>
@@ -75,85 +75,80 @@ export default class users extends Base {
                     <button class="danger-button">Make confirm contacts</button>
                 </div>
             </div>
-        `
-        this._qs('.users').innerHTML += data
-
-        //getprofilePicture
-        this.getprofilePicture(user.userId)
-    } //End of loadUser()
-
-    //View user account summary
-    async viewUser(id) {
-        this.setLoader()
-        await import('./subcomp/view-user/view-user.js')
-            .then(() => {
-                this._qs(
-                    '.popup'
-                ).innerHTML = `<view-user id="${id}"></view-user>`
-                this.stopLoader()
-            })
-            .catch(err => {
-                this.stopLoader()
-                dispatchEvent(
-                    new CustomEvent('pop-up', {
-                        detail: {
-                            pop: 'error',
-                            msg: err.message,
-                            duration:
-                                err.duration == undefined ? 10 : err.duration
-                        }
-                    })
-                )
-            })
-    } //End of viewUser()
+        `;
+    this._qs(".users").innerHTML += data;
 
     //getprofilePicture
-    async getprofilePicture(userId) {
-        try {
-            const res = await axios.post(
-                `${this.host}/images/profile/get/${userId}`,
-                {
-                    userId: this.getUserId(),
-                    token: this.getToken()
-                }
-            )
-            this._qs(`#img-${userId}`).src =
-                res.data.image != ''
-                    ? res.data.image
-                    : '/assets/img/alt/no-mage.png'
-        } catch (err) {
-            console.log(err)
+    this.getprofilePicture(user.userId);
+  } //End of loadUser()
+
+  //View user account summary
+  async viewUser(id) {
+    this.setLoader();
+    await import("./subcomp/view-user/view-user.js")
+      .then(() => {
+        this._qs(".popup").innerHTML = `<view-user id="${id}"></view-user>`;
+        this.stopLoader();
+      })
+      .catch((err) => {
+        this.stopLoader();
+        dispatchEvent(
+          new CustomEvent("pop-up", {
+            detail: {
+              pop: "error",
+              msg: err.message,
+              duration: err.duration == undefined ? 10 : err.duration,
+            },
+          })
+        );
+      });
+  } //End of viewUser()
+
+  //getprofilePicture
+  async getprofilePicture(userId) {
+    try {
+      const res = await axios.post(
+        `${this.host}/images/profile/get/${userId}`,
+        {
+          userId: this.getUserId(),
+          token: this.getToken(),
         }
-    } //End of getprofilePicture()
+      );
+      this._qs(`#img-${userId}`).src =
+        res.data.image != "" ? res.data.image : "/assets/img/alt/no-mage.png";
+    } catch (err) {
+      console.log(err);
+    }
+  } //End of getprofilePicture()
 
-    //load view user component
-    loadViewUser() {
-        this._qsAll('.view-profile').forEach(item => {
-            item.addEventListener('click', () => this.viewUser(item.dataset.id))
-        })
-    } //end of loadViewUser()
+  //load view user component
+  loadViewUser() {
+    this._qsAll(".view-profile").forEach((item) => {
+      item.addEventListener("click", () => this.viewUser(item.dataset.id));
+    });
+  } //end of loadViewUser()
 
-    // getUsers from API
-    async getUsers() {
-        this.setLoader()
-        await axios
-            .post(`${this.host}/AdminUsers/all-users`, {
-                ...this.authData()
-            })
-            .then(res => {
-                res.data.forEach(user => {
-                    //Load user component
-                    this.loadUser(user)
+  // getUsers from API
+  async getUsers() {
+    this.setLoader();
+    try {
+      const res = await axios.post(`${this.host}/admin-users/all-users`, {
+        ...this.authData(),
+      });
+      res.data.forEach((user) => {
+        //Load user component
+        this.loadUser(user);
 
-                    //loadViewUser
-                    this.loadViewUser()
-                })
-            })
-            .catch(err => console.log(err))
-        this.stopLoader()
-    } //end of getUsers()
+        //loadViewUser
+        this.loadViewUser();
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    this.stopLoader();
+  } //end of getUsers()
 
-/*
+  /*
   //filterProperty()
   filterProperty() {
     this._qsAll(".button-link").forEach((item) => {
@@ -192,13 +187,13 @@ export default class users extends Base {
   } //Endof filterProperty()
 */
 
-    // connectedCallback
-    connectedCallback() {
-        // getUsers from API
-        this.getUsers()
-        //filterProperty()
-        this.filterProperty();
-    } //End of connectedCallback()
+  // connectedCallback
+  connectedCallback() {
+    // getUsers from API
+    this.getUsers();
+    //filterProperty()
+    //this.filterProperty();
+  } //End of connectedCallback()
 } //End of Class
 
-window.customElements.define('users-comp', users)
+window.customElements.define("users-comp", users);
