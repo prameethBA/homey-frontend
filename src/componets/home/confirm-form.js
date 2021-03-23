10ece3a6700ca800b7e26417ea7b5fb98dbdae20
Lakmal98 <lakmalepp@gmail.com>
2021-03-23 14:22:48 +0530

    add slight changes

--- src/componets/home/confirm-form.js
+++ src/componets/home/confirm-form.js
@@ -18,7 +18,6 @@ export default class Confirm extends Base {
     constructor() {
         super()
         this.mount()
-
         const getQueryStringParams = query => {
             return query
                 ? (/^[?#]/.test(query) ? query.slice(1) : query)
@@ -36,6 +35,7 @@ export default class Confirm extends Base {
         const data = getQueryStringParams(window.location.search)
 
         const confirmUser = async () => {
+            console.log(data)
             await axios
                 .post(`${this.host}/signup/confirm`, {
                     userId: data.id,
