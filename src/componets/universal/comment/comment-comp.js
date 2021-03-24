824804d0cbb9d9ec88e12ebdcb362b408c54f301
osanda bhanuranga rathnayaka <47395557+osanda-b@users.noreply.github.com>
2021-03-24 16:49:15 +0530

    comment componet css

--- src/componets/universal/comment/comment-comp.js
+++ src/componets/universal/comment/comment-comp.js
@@ -47,20 +47,20 @@ export default class Comment extends Base {
     `
 
     constructor() {
-        super()
-        this.mount()
-    } //End of constructor
+            super()
+            this.mount()
+        } //End of constructor
 
     //getprofilePicture
     async getprofilePicture() {
-        try {
-            const res = await axios.post(`${this.host}/images/profile/get`, {
-                userId: this.getUserId(),
-                token: this.getToken()
-            })
-            this._qs(
-                '#profile-picture'
-            ).innerHTML = `<img id="profile-picture-image"
+            try {
+                const res = await axios.post(`${this.host}/images/profile/get`, {
+                    userId: this.getUserId(),
+                    token: this.getToken()
+                })
+                this._qs(
+                    '#profile-picture'
+                ).innerHTML = `<img id="profile-picture-image"
               src="${
                   res.data.image != ''
                       ? res.data.image
@@ -68,87 +68,89 @@ export default class Comment extends Base {
               }" 
               alt="Profile picture"
               />`
-        } catch (err) {
-            console.log(err)
-        }
-    } //End of getprofilePicture()
+            } catch (err) {
+                console.log(err)
+            }
+        } //End of getprofilePicture()
 
     //add new comment
     addNewComment() {
-        this._qs('#submit').addEventListener('click', async () => {
-            try {
-                const feedback = this._qs('#feedback').value
-                const anonymous = this._qs('#comment-anonymous').checked ? 1 : 0
-                if (feedback == '') throw 'Empty comment'
-                const res = await axios.post(`${this.host}/feedback/add`, {
-                    ...this.authData(),
-                    propertyId: this.getParam('id'),
-                    feedback: feedback,
-                    anonymous: anonymous
-                })
+            this._qs('#submit').addEventListener('click', async() => {
+                try {
+                    const feedback = this._qs('#feedback').value
+                    const anonymous = this._qs('#comment-anonymous').checked ? 1 : 0
+                    if (feedback == '') throw 'Empty comment'
+                    const res = await axios.post(`${this.host}/feedback/add`, {
+                        ...this.authData(),
+                        propertyId: this.getParam('id'),
+                        feedback: feedback,
+                        anonymous: anonymous
+                    })
 
-                if (res.status == 201) {
-                    dispatchEvent(
-                        new CustomEvent('pop-up', {
-                            detail: {
-                                pop: 'success',
-                                msg: res.data.message,
-                                duration: 5
-                            }
-                        })
-                    )
+                    if (res.status == 201) {
+                        dispatchEvent(
+                            new CustomEvent('pop-up', {
+                                detail: {
+                                    pop: 'success',
+                                    msg: res.data.message,
+                                    duration: 5
+                                }
+                            })
+                        )
 
-                    await import('./subcomp/comment-box.js')
-                    const values = this._qs('#comments-new').innerHTML
-                    this._qs('#comments-new').innerHTML =
-                        `<comment-box data-data="${this.encode({
+                        await
+                        import ('./subcomp/comment-box.js')
+                        const values = this._qs('#comments-new').innerHTML
+                        this._qs('#comments-new').innerHTML =
+                            `<comment-box data-data="${this.encode({
                             feedback: feedback,
                             propertyId: this.getParam('id'),
                             image: this._qs('#profile-picture-image').src
                         })}"></comment-box>` + values
 
-                    this._qs('#feedback').value = ''
-                } else throw res.data
-            } catch (err) {
-                console.log(err)
-            }
-        })
-    } //End of addNewComment()
+                        this._qs('#feedback').value = ''
+                    } else throw res.data
+                } catch (err) {
+                    console.log(err)
+                }
+            })
+        } //End of addNewComment()
 
     //get comments
     async getComments() {
-        try {
-            const res = await axios.post(`${this.host}/feedback/get/all`, {
-                ...this.authData(),
-                propertyId: this.getParam('id')
-            })
+            try {
+                const res = await axios.post(`${this.host}/feedback/get/all`, {
+                    ...this.authData(),
+                    propertyId: this.getParam('id')
+                })
 
-            await import('./subcomp/comment-box.js')
-            res.data.forEach(item => {
-                this._qs(
-                    '#comments'
-                ).innerHTML += `<comment-box id=${item.id} view="true"></comment-box>`
-            })
-        } catch (err) {
-            console.log(err)
-        }
-    } //End of getComments()
+                await
+                import ('./subcomp/comment-box.js')
+                res.data.forEach(item => {
+                    this._qs(
+                        '#comments'
+                    ).innerHTML += `<comment-box id=${item.id} view="true"></comment-box>`
+                })
+            } catch (err) {
+                console.log(err)
+            }
+        } //End of getComments()
 
     connectedCallback() {
-        this._qs('#close').addEventListener(
-            'click',
-            () => (this._qs('.backdrop').style.display = 'none')
-        )
+            this._qs('#close').addEventListener(
+                'click',
+                () => (this._qs('.backdrop').style.display = 'none')
+            )
 
-        //get comments
-        this.getComments()
+            //get comments
+            this.getComments()
 
-        //getprofilePicture
-        this.getprofilePicture()
+            //getprofilePicture
+            this.getprofilePicture()
 
-        //add new comment
-        this.addNewComment()
-    } //End of connectedCallback
+            //add new comment
+            this.addNewComment()
+        } //End of connectedCallback
 } //End of class
 
 window.customElements.define('comment-comp', Comment)
